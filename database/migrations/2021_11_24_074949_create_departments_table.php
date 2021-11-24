49b470cc5dcb42871b612a56de0cfe1467d3a436
luckykun88 <kmisigaro@outlook.com>
2021-11-24 10:55:28 +0300

    addition of departments and department users table

--- database/migrations/2021_11_24_074949_create_departments_table.php
+++ database/migrations/2021_11_24_074949_create_departments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasksTable extends Migration
+class CreateDepartmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->id();
-            $table->string('task_name');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->longText('report');
-            $table->date('start_date');
-            $table->date('end_date');
-            $table->longText('comment');
-            $table->integer('status')->default(1); //1-->in progress, 2-->overdue, 3-->complete
+            $table->unsignedBigInteger('department_id');
+            $table->foreign('department_id')->references('id')->on('departments');
             $table->timestamps();
         });
     }
@@ -34,6 +30,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('departments');
     }
 }
